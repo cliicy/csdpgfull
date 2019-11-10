04c6b6563f9c7a89e95298b69d0c28e8338b865e
cliicy <cliicy@gmail.com>
2019-11-10 07:22:38 -0800

    fio full

--- intel3.2t/fulldisk-fio_postgre_run-case.sh
+++ intel3.2t/fulldisk-fio_postgre_run-case.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+## re-use vada's prepared and vacuumed data to save time
+#mnt_opt=" -o noatime,nodiratime"
+#mnt_point_vanda_data="/opt/data/vanda"
+#sudo umount /dev/sfdv0n1
+#sudo mkfs -t xfs -f /dev/sfdv0n1
+#sudo mount /dev/sfdv0n1 ${mnt_opt} ${mnt_point_vanda_data}_2TBK
+#cp -r ${mnt_point_vanda_data}bk/postgresql-10.10 ${mnt_point_vanda_data}_2TBK/
+
+
 sed -i s/fillfact=75/fillfact=100/ ./sysbench-cfg/benchmark.cfg
 ./fulldisk_precond_1_prep_dev.sh ./sysbench-cfg/benchmark.cfg 
 
