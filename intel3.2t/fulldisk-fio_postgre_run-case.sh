91d305254ef089f65b579b50b93ad4d8f2cbffa8
cliicy <cliicy@gmail.com>
2019-11-11 01:32:47 -0800

    update intel's fio config

--- intel3.2t/fulldisk-fio_postgre_run-case.sh
+++ intel3.2t/fulldisk-fio_postgre_run-case.sh
@@ -24,11 +24,11 @@ popd
 
 
 ####fillfact75
-sleep 10
-sed -i s/fillfact=100/fillfact=75/ ./sysbench-cfg/benchmark.cfg
-./fulldisk_precond_1_prep_dev.sh ./sysbench-cfg/benchmark.cfg
-./2_initdb.sh   ./sysbench-cfg/benchmark.cfg
-./3_run.sh      ./sysbench-cfg/benchmark.cfg
+#sleep 10
+#sed -i s/fillfact=100/fillfact=75/ ./sysbench-cfg/benchmark.cfg
+#./fulldisk_precond_1_prep_dev.sh ./sysbench-cfg/benchmark.cfg
+#./2_initdb.sh   ./sysbench-cfg/benchmark.cfg
+#./3_run.sh      ./sysbench-cfg/benchmark.cfg
 
-./do2runwkloads.sh ./sysbench-cfg/benchmark.cfg
+#./do2runwkloads.sh ./sysbench-cfg/benchmark.cfg
 
